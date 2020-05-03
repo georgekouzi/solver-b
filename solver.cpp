78b5ef2e17c7a9ec14bb985e8db91f7c8d9096a7
georgekouzi <57435555+georgekouzi@users.noreply.github.com>
2020-05-04 02:34:53 +0300

    finish

--- solver.cpp
+++ solver.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <string>
-
 #include <math.h>
 #include "solver.hpp"
 using namespace std;
@@ -8,15 +6,9 @@ namespace solver{
 
 //////OP-
  const  RealVariable operator- ( const RealVariable &t, const RealVariable& t1){
-    
-      cout<<"friend const  RealVariable operator- ( const RealVariable &t, const RealVariable& t1)"<<endl;
-cout<<"t= "<<t<<endl;
-cout<<"t1= "<<t1<<endl;
 if (t.num_rg()!=0){
  
- cout<<"if " <<RealVariable(t1.num_rg(),t.num_rg()-t1.c(),t1.b(),t1.a())<<endl;
  if(t1.b()>=0){
-cout<<"in if on if "<<endl;
  
  if(t1.a()!=0&&t1.b()!=0){
     return   RealVariable(t1.num_rg(),t.num_rg()-t1.c(),-t1.b(),-t1.a());
@@ -29,62 +21,41 @@ cout<<"in if on if "<<endl;
 
  }
  else{
-   cout<<"in else on if "<<endl;
     return   RealVariable(t1.num_rg(),t.num_rg()-t1.c(),t1.b(),t1.a());
 }
 }
-
 else if(t1.num_rg()!=0){
-    cout<< "else if "<<RealVariable(t.num_rg(),t.c()-t1.num_rg(),t.b(),t.a())<<endl;
     return   RealVariable(t.num_rg(),t.c()-t1.num_rg(),t.b(),t.a());
 }
 
-else
-    cout<< "else"<<RealVariable(t.num_rg(),t.c()-t1.c(),t.b()-t1.b(),t.a())<<endl;
     return   RealVariable(t.num_rg(),t.c()-t1.c(),t.b()-t1.b(),t.a()-t1.a());
-
-  }
+}
+  
  ///op+
  
 const  RealVariable operator+ ( const RealVariable &t, const RealVariable& t1){
     
-      cout<<"friend const  RealVariable operator+ ( const RealVariable &t, const RealVariable& t1)"<<endl;
-cout<<"t= "<<t<<endl;
-cout<<"t1= "<<t1<<endl;
-// cout<< RealVariable( t1.a(),t1.b(),t1.c()+t.a())<<endl;
+     
 if (t.num_rg()!=0){
-  cout<<"t.num_rg()!=0"<<endl;
- cout<< RealVariable( t1.num_rg(),t1.c()+t.num_rg(),t1.b(),t1.a())<<endl;
-
     return   RealVariable( t1.num_rg(),t1.c()+t.num_rg(),t1.b(),t1.a());
 }
 else if(t1.num_rg()!=0){
-  cout<<"t1.num_rg()!=0"<<endl;
-  cout<<RealVariable(t.num_rg(),t.c()+t1.num_rg(),t.b(),t.a())<<endl;
-
     return   RealVariable(t.num_rg(),t.c()+t1.num_rg(),t.b(),t.a());
 }
  else
-   cout<< "else "<<RealVariable( t.num_rg(),t.c()+t1.c(),t.b()+t1.b(),t.a()+t1.a())<<endl;
-
          return   RealVariable( t.num_rg(),t.c()+t1.c(),t.b()+t1.b(),t.a()+t1.a());
 
 }
  
 // op*
    RealVariable operator*(double num, const RealVariable& t){
-    // cout<<num<<endl;
-
-       cout<<"friend   RealVariable operator*(double num, RealVariable& t)"<<endl;
-      cout<<"t= "<<t<<endl;
+  
 if(t.a()!=0){
-    cout<<"new complex= "<< RealVariable(t.num_rg(),t.c(),t.b(),t.a()+num-1)<<endl;
 return RealVariable(t.num_rg(),t.c(),t.b(),t.a()+num-1);
 
 }
 
 else
-cout<<"new complex= "<< RealVariable(t.num_rg(),t.c(),t.b()+num-1,t.a())<<endl;
 return RealVariable(t.num_rg(),t.c(),t.b()+num-1,t.a());
    }
  
@@ -93,32 +64,32 @@ return RealVariable(t.num_rg(),t.c(),t.b()+num-1,t.a());
 RealVariable operator/(const RealVariable& t ,double num){
        if(num==0){
              throw invalid_argument("no real solution ");
-
        }
-       
-       cout<<"friend   RealVariable operator/(const RealVariable& t ,double num)"<<endl;
 if(t.a()!=0){
-    cout<<"new complex= "<< RealVariable(t.num_rg(),t.c(),t.b(),t.a()/num)<<endl;
 return RealVariable(t.num_rg(),t.c(),t.b(),t.a()/num);
 
 }
-
 else
-cout<<"new complex= "<< RealVariable(t.num_rg(),t.c(),t.b()/num,t.a())<<endl;
 return RealVariable(t.num_rg(),t.c(),t.b()/num,t.a());
    }
- 
+ //op^
+  RealVariable operator^(const RealVariable& t, int num){
+             if(num!=2){
+              throw invalid_argument("no real solution ");
+            }
+              return RealVariable(t._num_rg,t._c,t._b-1,t._a+1);
+             
+}
  ///func solve
  double solve(const RealVariable& t){
 double resultPluse;
-double resultMinus;
+// double resultMinus;
 if(t.a()==0&&t.b()==0){
     throw invalid_argument("no real solution ");
 }
 if(t.a()!=0){
 resultPluse=(-t.b()+sqrt(t.b()*t.b()-4*t.a()*t.c()))/(2*t.a());
-resultMinus=(-t.b()-sqrt(t.b()*t.b()-4*t.a()*t.c()))/(2*t.a());
-cout<<"result= "<< resultPluse<<endl;
+//resultMinus=(-t.b()-sqrt(t.b()*t.b()-4*t.a()*t.c()))/(2*t.a());
 if(isnan(resultPluse)){
     throw invalid_argument("no real solution ");
 }
@@ -127,24 +98,15 @@ return resultPluse;
 }
 else
 resultPluse=(t.c()*-1)/t.b();
-
-cout<<"result= "<< resultPluse;
-
 return resultPluse;
 }
   
   ///////////ComplexVariable////////////
    //////OP-
  const  ComplexVariable operator- ( const ComplexVariable &t, const ComplexVariable& t1){
-    
-      cout<<"friend const  ComplexVariable operator- ( const ComplexVariable &t, const ComplexVariable& t1)"<<endl;
-cout<<"t= "<<t<<endl;
-cout<<"t1= "<<t1<<endl;
 if (t.num_rg()!=0){
  
- cout<<"if " <<ComplexVariable(t1.num_rg(),t.num_rg()-t1.c(),t1.b(),t1.a(),t.i())<<endl;
- if(t1.b()>=0){
-cout<<"in if on if "<<endl;
+  if(t1.b()>=0){
  
  if(t1.a()!=0&&t1.b()!=0){
     return   ComplexVariable(t1.num_rg(),t.num_rg()-t1.c(),-t1.b(),-t1.a(),t1._i);
@@ -157,64 +119,35 @@ cout<<"in if on if "<<endl;
 
  }
  else{
-   cout<<"in else on if "<<endl;
     return   ComplexVariable(t1.num_rg(),t.num_rg()-t1.c(),t1.b(),t1.a(),t1._i);
 }
 }
-
 else if(t1.num_rg()!=0){
-    cout<< "else if "<<ComplexVariable(t.num_rg(),t.c()-t1.num_rg(),t.b(),t.a(),t.i())<<endl;
     return   ComplexVariable(t.num_rg(),t.c()-t1.num_rg(),t.b(),t.a());
 }
-
-else
-    cout<< "else"<<ComplexVariable(t.num_rg(),t.c()-t1.c(),t.b()-t1.b(),t.a(),t.i())<<endl;
     return   ComplexVariable(t.num_rg(),t.c()-t1.c(),t.b()-t1.b(),t.a()-t1.a(),t.i());
-
   }
  ///op+
  
 const  ComplexVariable operator+ ( const ComplexVariable &t, const ComplexVariable& t1){
     
-      cout<<"friend const  ComplexVariable operator+ ( const ComplexVariable &t, const ComplexVariable& t1)"<<endl;
-cout<<"t= "<<t<<endl;
-cout<<"t1= "<<t1<<endl;
-// cout<< ComplexVariable( t1.a(),t1.b(),t1.c()+t.a())<<endl;
 if (t.num_rg()!=0){
-  cout<<"t.num_rg()!=0"<<endl;
- cout<< ComplexVariable( t1.num_rg(),t1.c()+t.num_rg(),t1.b(),t1.a(),t.i())<<endl;
-
     return   ComplexVariable( t1.num_rg(),t1.c()+t.num_rg(),t1.b(),t1.a(),t.i());
 }
 else if(t1.num_rg()!=0){
-  cout<<"t1.num_rg()!=0"<<endl;
-  cout<<ComplexVariable(t.num_rg(),t.c()+t1.num_rg(),t.b(),t.a(),t.i())<<endl;
-
     return   ComplexVariable(t.num_rg(),t.c()+t1.num_rg(),t.b(),t.a(),t.i());
 }
  else
-   cout<< "else "<<ComplexVariable( t.num_rg(),t.c()+t1.c(),t.b()+t1.b(),t.a()+t1.a(),t.i())<<endl;
-
-         return   ComplexVariable( t.num_rg(),t.c()+t1.c(),t.b()+t1.b(),t.a()+t1.a(),t.i());
+    return   ComplexVariable( t.num_rg(),t.c()+t1.c(),t.b()+t1.b(),t.a()+t1.a(),t.i());
 
 }
  
 // op*
    ComplexVariable operator*(double num, const ComplexVariable& t){
-    // cout<<num<<endl;
-
-       cout<<"friend   ComplexVariable operator*(double num, ComplexVariable& t)"<<endl;
-      cout<<"t= "<<t<<endl;
-            cout<<"t= "<<t.c()<<endl;
-
 if(t.a()!=0){
-    cout<<"new complex= "<< ComplexVariable(t.num_rg(),t.c(),t.b(),t.a()+num-1,t.i())<<endl;
 return ComplexVariable(t.num_rg(),t.c(),t.b(),t.a()+num-1,t.i());
-
 }
-
 else
-cout<<"new complex= "<< ComplexVariable(t.num_rg(),t.c(),t.b()+num-1,t.a(),t.i())<<endl;
 return ComplexVariable(t.num_rg(),t.c(),t.b()+num-1,t.a(),t.i());
    }
  
@@ -223,21 +156,23 @@ return ComplexVariable(t.num_rg(),t.c(),t.b()+num-1,t.a(),t.i());
 ComplexVariable operator/(const ComplexVariable& t ,double num){
        if(num==0){
              throw invalid_argument("no real solution ");
-
        }
-       
-       cout<<"friend   ComplexVariable operator/(const ComplexVariable& t ,double num)"<<endl;
 if(t.a()!=0){
-    cout<<"new complex= "<< ComplexVariable(t.num_rg(),t.c(),t.b(),t.a()/num,t.i())<<endl;
 return ComplexVariable(t.num_rg(),t.c(),t.b(),t.a()/num,t.i());
 
 }
-
 else
-cout<<"new complex= "<< ComplexVariable(t.num_rg(),t.c(),t.b()/num,t.a(),t.i())<<endl;
 return ComplexVariable(t.num_rg(),t.c(),t.b()/num,t.a(),t.i());
    }
-
+//op^
+ const ComplexVariable operator^(const ComplexVariable& t, int num){
+       if(num!=2){
+        throw invalid_argument("pow need to be ^2");
+       }
+       else
+      return ComplexVariable(t._num_rg,t._c,t._b-1,t._a+1,t._i);
+    
+    }
 
 
 complex<double> solve( const ComplexVariable& t) {
@@ -250,25 +185,19 @@ if(isnan(result)){
 
 result=(-t.b()+sqrt((t.b()*t.b()-4*t.a()*t.c())*-1))/(2*t.a());;
 i=i+result;
-cout<<std::complex<double>(0,i)<<endl;
 return std::complex<double>(0,i);
 }
 else{ 
-cout<<std::complex<double>(result,i)<<endl;
 return std::complex<double>(result,i);
 }
 }
 else{
   result=(t.c()*-1)/t.b();
-cout<<std::complex<double>(result,i)<<endl;
 return std::complex<double>(result,i);
 }
 
         }
- 
- 
- 
- 
+
  
  };
  
--- solver.hpp
+++ solver.hpp
@@ -4,6 +4,7 @@
 using namespace std;
 
  namespace solver{
+
 class RealVariable {
 
 private:
@@ -35,26 +36,20 @@ public:
 
 
 //op -
- RealVariable operator-(double num)  {
-         cout<<"ppfffff ---"<<RealVariable(_num_rg,_c-num,_b,_a)<<endl;      
+       RealVariable operator-(double num)  {
         return RealVariable(_num_rg,_c-num,_b,_a);
      }
     
-    friend   RealVariable operator-(double num,  RealVariable& t){
-    cout<<RealVariable(t._num_rg,t._c+num,-t._b,t._a)<<endl;
+    friend   RealVariable operator-(double num, const RealVariable& t){
     return RealVariable(t._num_rg,t._c+num,-t._b,t._a);
   }
   friend const  RealVariable operator- ( const RealVariable &t, const RealVariable& t1);
 //op+
-friend   RealVariable operator+(double num,  RealVariable& t){
-  cout<<"friend   RealVariable operator+(double num,  RealVariable& t)"<<endl<<"t= "<<t<<endl<<RealVariable(t._num_rg,t._c+num,t._b,t._a)<<endl;
+friend   const RealVariable operator+(double num, const RealVariable& t){
 return RealVariable(t._num_rg,t._c+num,t._b,t._a);
   }
    
-   friend  RealVariable operator+(RealVariable& t, double num ){
-            cout<<"friend  RealVariable operator+(RealVariable& t, double num )"<<endl;
-      cout<<"t= "<<t<<endl;
-    cout<<RealVariable(t._num_rg,t._c+num,t._b,t._a)<<endl;
+   friend  RealVariable operator+(const RealVariable& t, double num ){
 return RealVariable(t._num_rg,t._c+num,t._b,t._a);
 
    }
@@ -67,50 +62,19 @@ friend RealVariable operator/(const RealVariable& t ,double num);
 //op==
 
 friend  RealVariable operator==(const RealVariable& t, double num){
-       cout<<" friend  RealVariable operator==(const RealVariable& t, double num)"<<endl;
-       cout<<"t= "<<t<<endl;
-       //cout<<"t1= "<<t1<<endl;
-cout<<"new complex= "<<RealVariable(0,t._c+num,t._b,t._a)<<endl;
-          return RealVariable(0,t._c-num,t._b,t._a)   ;
+          return RealVariable(0,t._c-num,t._b,t._a);
    }
 
 friend  RealVariable operator==(double num, const RealVariable& t){
-       cout<<"friend  RealVariable operator==(double num, const RealVariable& t)"<<endl;
-       cout<<"t= "<<t<<endl;
-       //cout<<"t1= "<<t1<<endl;
-cout<<"new complex= "<<RealVariable(0,t._c+num,t._b,t._a)<<endl;
-          return RealVariable(0,t._c-num,t._b,t._a)   ;
+                return RealVariable(0,t._c-num,t._b,t._a);
    }
 
-friend   RealVariable operator==(const RealVariable& t,const RealVariable& t1){
-       cout<<"friend   RealVariable operator==(const RealVariable& t,const RealVariable& t1"<<endl;
-       cout<<"t= "<<t<<endl;
-       cout<<"t1= "<<t1<<endl;
-       cout<<"new complex= "<<RealVariable(0,t._c-t1._c,t._b-t1._b,t._a-t1._a)<<endl;
-          return RealVariable(0,t._c-t1._c,t._b-t1._b,t._a-t1._a)   ;
+friend  RealVariable operator==(const RealVariable& t,const RealVariable& t1){
+          return RealVariable(0,t._c-t1._c,t._b-t1._b,t._a-t1._a);
    }
 
 //op^
-friend   RealVariable operator^(RealVariable& t, int num){
-    //  cout<<num<<endl;
-    //    cout<<"friend   RealVariable operator^(double num, RealVariable& t)"<<RealVariable(t._a,t._b+num-1,t._c)<<endl;
-             if(num!=2){
-              throw invalid_argument("no real solution ");
-             }
-         
-             else
-                  cout<<"friend   RealVariable operator^(RealVariable& t, int num)"<<endl;
-                  cout<<"t= "<<t<<endl;
-              cout<<"new complex= "<<RealVariable(t._num_rg,t._c,t._b-1,t._a+1)<<endl;
-                   return RealVariable(t._num_rg,t._c,t._b-1,t._a+1);
-    }
-///for check
- friend ostream& operator<< (ostream& os, const RealVariable& c){
-               // return (os <<"A= "<< c._a <<", B= " << c._b<<", C= " <<c._c);
-               return (os<<"_num_rg= "<<c._num_rg <<", A= "<< c._a <<", B= " << c._b<<", C= " <<c._c);
-
-    }
-
+friend  RealVariable operator^(const RealVariable& t, int num);
 };
 
 class ComplexVariable{
@@ -145,27 +109,19 @@ public:
 	} 
 //op -
  ComplexVariable operator-(double num)  {
-         cout<<"ppfffff ---"<<ComplexVariable(_num_rg,_c-num,_b,_a,_i)<<endl;      
         return ComplexVariable(_num_rg,_c-num,_b,_a,_i);
      }
-    
-    friend   ComplexVariable operator-(double num,  ComplexVariable& t){
-    cout<<ComplexVariable(t._num_rg,t._c+num,-t._b,t._a,t._i)<<endl;
+        friend   ComplexVariable operator-(double num, const ComplexVariable& t){
     return ComplexVariable(t._num_rg,t._c+num,-t._b,t._a,t._i);
   }
   friend const  ComplexVariable operator- ( const ComplexVariable &t, const ComplexVariable& t1);
 //op+
 friend   ComplexVariable operator+(double num,const  ComplexVariable& t){
-  cout<<"friend   ComplexVariable operator+(double num,  ComplexVariable& t)"<<endl<<"t= "<<t<<endl<<ComplexVariable(t._num_rg,t._c+num,t._b,t._a,t._i)<<endl;
 return ComplexVariable(t._num_rg,t._c+num,t._b,t._a,t._i);
   }
    
    friend  ComplexVariable operator+(const ComplexVariable& t, double num ){
-            cout<<"friend  ComplexVariable operator+(ComplexVariable& t, double num )"<<endl;
-      cout<<"t= "<<t<<endl;
-    cout<<ComplexVariable(t._num_rg,t._c+num,t._b,t._a,t._i)<<endl;
 return ComplexVariable(t._num_rg,t._c+num,t._b,t._a,t._i);
-
    }
 
 friend const  ComplexVariable operator+ ( const ComplexVariable& t, const ComplexVariable& t1);
@@ -176,61 +132,28 @@ friend ComplexVariable operator/(const ComplexVariable& t ,double num);
 //op==
 
 friend  ComplexVariable operator==(const ComplexVariable& t, double num){
-       cout<<" friend  ComplexVariable operator==(const ComplexVariable& t, double num)"<<endl;
-       cout<<"t= "<<t<<endl;
-       //cout<<"t1= "<<t1<<endl;
-cout<<"new complex= "<<ComplexVariable(0,t._c+num,t._b,t._a,t._i)<<endl;
-          return ComplexVariable(0,t._c-num,t._b,t._a,t._i)   ;
+          return ComplexVariable(0,t._c-num,t._b,t._a,t._i);
    }
 
 friend  ComplexVariable operator==(double num, const ComplexVariable& t){
-       cout<<"friend  ComplexVariable operator==(double num, const ComplexVariable& t)"<<endl;
-       cout<<"t= "<<t<<endl;
-       //cout<<"t1= "<<t1<<endl;
-cout<<"new complex= "<<ComplexVariable(0,t._c+num,t._b,t._a,t._i)<<endl;
           return ComplexVariable(0,t._c-num,t._b,t._a,t._i)   ;
    }
 
 friend   ComplexVariable operator==(const ComplexVariable& t,const ComplexVariable& t1){
-       cout<<"friend   ComplexVariable operator==(const ComplexVariable& t,const ComplexVariable& t1"<<endl;
-       cout<<"t= "<<t<<endl;
-       cout<<"t1= "<<t1<<endl;
-       cout<<"new complex= "<<ComplexVariable(0,t._c-t1._c,t._b-t1._b,t._a-t1._a,t._i-t1._i)<<endl;
           return ComplexVariable(0,t._c-t1._c,t._b-t1._b,t._a-t1._a,t._i-t1._i)   ;
    }
 
 //op^
-friend const   ComplexVariable operator^(const ComplexVariable& t, int num){
-    //  cout<<num<<endl;
-    //    cout<<"friend   ComplexVariable operator^(double num, ComplexVariable& t)"<<ComplexVariable(t._a,t._b+num-1,t._c)<<endl;
-       if(num!=2){
-        throw invalid_argument("pow need to be ^2");
-       }
-       else
-       cout<<"friend   ComplexVariable operator^(ComplexVariable& t, int num)"<<endl;
-       cout<<"t= "<<t<<endl;
-cout<<"new complex= "<<ComplexVariable(t._num_rg,t._c,t._b-1,t._a+1,t._i)<<endl;
- return ComplexVariable(t._num_rg,t._c,t._b-1,t._a+1,t._i);
-       
-    
+friend const   ComplexVariable operator^(const ComplexVariable& t, int num);
 
-    }
-///for check
- friend ostream& operator<< (ostream& os, const ComplexVariable& c){
-               // return (os <<"A= "<< c._a <<", B= " << c._b<<", C= " <<c._c);
-               return (os<<"_num_rg= "<<c._num_rg <<", A= "<< c._a <<", B= " << c._b<<", C= " <<c._c<<", i= "<<c._i);
-
-    }
  friend const  ComplexVariable operator+ ( const ComplexVariable &t, std::complex<double>num){
-   cout<<num.imag()<<endl;
         return   ComplexVariable(t._num_rg,t._c,t._b,t._a,t._i+num.imag());
 
     }
 
-
 };
+
 double solve(const  RealVariable&t);
 complex<double> solve(const ComplexVariable&t);
-
 
 };
